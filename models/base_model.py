825c407333b4a4fadfe5528d76be6b3a3a1604c5
saneleM1991 <91sanelem@gmail.com>
2023-10-16 12:27:16 +0200

    base_model constructor fixed

--- models/base_model.py
+++ models/base_model.py
@@ -18,14 +18,21 @@ class BaseModel:
         self.updated_at = datetime.now()
 
         if len(kwargs) != 0:
+            del kwargs["__class__"]
+
             self.id = kwargs["id"]
             self.created_at = datetime.fromisoformat(kwargs["created_at"])
             self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
+
+            for key in kwargs.keys():
+                if key not in self.__dict__.keys():
+                    setattr(self, key, kwargs[key])
         else:
             models.storage.new(self)
 
     def __str__(self):
         """Return string representation of `BaseModel` class."""
+        print(self.__dict__.keys())
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
@@ -39,4 +46,4 @@ class BaseModel:
         d["__class__"] = type(self).__name__
         d["created_at"] = self.created_at.isoformat()
         d["updated_at"] = self.updated_at.isoformat()
-        return
+        return d
